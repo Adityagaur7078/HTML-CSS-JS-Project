74b9049811aad8354ddf3f9f51e49718a6e9b64b
Adityagaur7078 <adityagaur7078@gmail.com>
2026-03-17 23:19:01 +0530

    Add delete feature and persist notes using localStorage

--- Notes App/script.js
+++ Notes App/script.js
@@ -2,16 +2,47 @@ const input = document.getElementById("noteInput");
 const button = document.getElementById("addBtn");
 const container = document.getElementById("notesContainer");
 
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+function renderNotes(){
+container.innerHTML = "";
+
+notes.forEach((note, index) => {
+
+const div = document.createElement("div");
+div.classList.add("note");
+
+const text = document.createElement("p");
+text.textContent = note;
+
+const delBtn = document.createElement("button");
+delBtn.textContent = "Delete";
+
+delBtn.onclick = () => {
+notes.splice(index,1);
+localStorage.setItem("notes", JSON.stringify(notes));
+renderNotes();
+};
+
+div.appendChild(text);
+div.appendChild(delBtn);
+
+container.appendChild(div);
+
+});
+}
+
 button.addEventListener("click", () => {
 
 if(input.value.trim() === "") return;
 
-const div = document.createElement("div");
-div.classList.add("note");
-div.textContent = input.value;
+notes.push(input.value);
 
-container.appendChild(div);
+localStorage.setItem("notes", JSON.stringify(notes));
 
 input.value = "";
 
+renderNotes();
 });
+
+renderNotes();
